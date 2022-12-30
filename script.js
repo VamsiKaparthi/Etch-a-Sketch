9cbc610411c5c5197785edcf915eb8f9dc8efe41
Vamsi <vk784@snu.edu.in>
2022-12-30 18:53:11 +0530

    Add hover effect to grid elements

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const container = document.getElementById('c');
 
-let grid = document.createElement("div");
-grid.style.backgroundColor = "white";
-function gridMaker(n){
-    grid.style.width = `calc(600px/${n})`;
-    grid.style.height = `calc(600px/${n})`;
-    for(let i = 0 ; i< n**2 ; i++){
-        container.appendChild(grid.cloneNode(true));
-    }
-    
 
+
+
+function gridMaker(n){
+    for(let i = 0 ; i< n**2 ; i++){
+        let grid = document.createElement("div");
+        grid.style.backgroundColor = "white";
+        grid.style.width = `calc(600px/${n})`;
+        grid.style.height = `calc(600px/${n})`;
+        grid.addEventListener('mouseenter',()=>{
+            grid.style.backgroundColor = "black";
+        });
+        container.appendChild(grid);
+    }
 }
-gridMaker(16);
+gridMaker(2);
