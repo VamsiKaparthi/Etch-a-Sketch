275777c193fb26cf76e3844c6b1d40154fbe9d66
Vamsi <vk784@snu.edu.in>
2022-12-30 17:33:52 +0530

    Add grid as a test element

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const container = document.getElementById('c');
-let grid = 
+
+let grid = document.createElement("div");
+grid.style.backgroundColor = "red";
+grid.style.height = "100px";
+grid.style.width = "100px";
