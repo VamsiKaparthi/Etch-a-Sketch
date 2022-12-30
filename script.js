9597a6c8442ad5eacbb4e24ff7b4a008c8f2748c
Vamsi <vk784@snu.edu.in>
2022-12-30 17:56:44 +0530

    Make container display flex, add flex-wrap and remove garbage grid class

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const container = document.getElementById('c');
 
 let grid = document.createElement("div");
 grid.style.backgroundColor = "red";
-
 function gridMaker(n){
     grid.style.width = `calc(600px/${n})`;
     grid.style.height = `calc(600px/${n})`;
